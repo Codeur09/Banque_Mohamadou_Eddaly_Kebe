6050b195dd995b3d88cf999ecc3b8d32e521c535
Mohamadou Eddaly KEBE <mohamadou-eddaly1.kebe@epita.fr>
2025-11-20 11:52:55 +0100

    Ajout dépendances Spring Web, JPA et H2

---
+++ Banque_Mohamadou_Eddaly_Kebe/src/main/java/org/example/banque_mohamadou_eddaly_kebe/BanqueMohamadouEddalyKebeApplication.java
@@ -0,0 +1,13 @@
+package org.example.banque_mohamadou_eddaly_kebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanqueMohamadouEddalyKebeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanqueMohamadouEddalyKebeApplication.class, args);
+    }
+
+}
---
+++ Banque_Mohamadou_Eddaly_Kebe/src/test/java/org/example/banque_mohamadou_eddaly_kebe/BanqueMohamadouEddalyKebeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.banque_mohamadou_eddaly_kebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanqueMohamadouEddalyKebeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
